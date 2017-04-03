4ec42d8ec555f47808c240580044d8104a1b2342
Joanna Diong <joanna.diong@gmail.com>
2017-04-03 14:25:23 +1000

    spectral: add find_index_digital()

--- spectral.py
+++ spectral.py
@@ -2,11 +2,11 @@ import numpy as np
 from numpy import trapz
 
 
-def find_index(f, lowpass, band_ll, band_ul):
+def find_index_fband(f, lowpass, band_ll, band_ul):
     """
     Find the start and stop indices and values of a nominated frequency bandwith to calculate
     the proportion of spectral power for that bandwidth.
-    This function requires a spectral analysis is first performed on the recorded signal.
+    This function requires a power spectral density analysis is first performed on the recorded signal.
 
     Example:
         import numpy as np
@@ -17,7 +17,7 @@ def find_index(f, lowpass, band_ll, band_ul):
         x = filter_lowpass(x, 1000, 40)
         f, Pxx_den = signal.welch(x, 1000, nperseg=250)
         plt.plot(f, Pxx_den)
-        start_idx, start_val, stop_idx, stop_val = find_index(f, 40, 8, 12)
+        start_idx, start_val, stop_idx, stop_val = find_index_fband(f, 40, 8, 12)
 
     :param f: sample frequencies
     :type f: ndarray
@@ -30,7 +30,7 @@ def find_index(f, lowpass, band_ll, band_ul):
     :return: start and stop indices and values
     :rtype: int or float
     """
-    def find_nearest(f, lowpass, limit):
+    def find_index_nearest(f, lowpass, limit):
         """
         In an array of frequencies, find the nearest index and value of the nominated frequency.
 
@@ -46,15 +46,15 @@ def find_index(f, lowpass, band_ll, band_ul):
         assert limit <= lowpass, 'Error: The nominated frequency is greater than the low pass cut-off'
         idx = (np.abs(f - limit)).argmin()
         return idx, f[idx]
-    start_idx, start_val = find_nearest(f, lowpass, band_ll)
-    stop_idx, stop_val = find_nearest(f, lowpass, band_ul)
+    start_idx, start_val = find_index_nearest(f, lowpass, band_ll)
+    stop_idx, stop_val = find_index_nearest(f, lowpass, band_ul)
     return start_idx, start_val, stop_idx, stop_val
 
 
 def calc_percentpower(Pxx_den, start_idx, stop_idx):
     """
     Calculate proportion of spectral power in a recorded signal over a nominated frequency bandwidth.
-    This function requires a spectral analysis is first performed on the recorded signal.
+    This function requires a power spectral density analysis is first performed on the recorded signal.
 
     Example:
         import numpy as np
@@ -82,5 +82,25 @@ def calc_percentpower(Pxx_den, start_idx, stop_idx):
     percentpower = area_band / area_full * 100
     print('Proportion of power in bandwidth (%): {:.3f}'.format(percentpower))
     return percentpower
+
+
+def find_index_digital(array, value):
+    """
+    Find indices when a digital signal changes from LOW to HIGH, and HIGH to LOW.
+
+    :param array: 1D array of digital output eg. 0 and 1
+    :type array: ndarray
+    :param value: HIGH value eg. 1
+    :type value: int
+    :return: start and stop indices
+    :rtype: list
+    """
+    start_idx, stop_idx = [], []
+    for i in range(array.size):
+        if array[i] - array[i - value] == value:
+            start_idx.append(i)
+        if array[i] - array[i - value] == -value:
+            stop_idx.append(i)
+    return start_idx, stop_idx
 
 
